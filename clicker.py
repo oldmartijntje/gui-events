04983ece36f8319bc4ee446480ce78c0b1b4c3d8
oldmartijntje <oldmartijntje@gmai.com>
2022-02-03 12:10:02 +0100

    done

--- clicker.py
+++ clicker.py
@@ -3,30 +3,43 @@ window = tkinter.Tk()
 string = tkinter.StringVar(value='0')
 amount = 0
 window.configure(bg = 'grey')
+touching = False
 
-
-def change(change):
-    global amount
-    amount = change
-    string.set(amount)
-    if amount > 0:
+def changeColor():
+    if touching == True:
+        pass
+    elif amount > 0:
         window.configure(bg = 'green')
     elif amount < 0:
         window.configure(bg = 'red')
     else:
         window.configure(bg = 'grey')
 
+def change(change):
+    global amount
+    amount = change
+    string.set(amount)
+    changeColor()
 
+def enter(event):
+    global touching
+    touching = True
+    window.configure(bg = 'yellow')
 
+def leave(event):
+    global touching
+    touching = False
+    changeColor()
 
 button1 = tkinter.Button(window)
 button1.configure(text='Up', command= lambda: change(amount + 1))
 button1.pack(ipady=10, fill = 'x', padx = 10, pady = 10)
 
 button2 = tkinter.Button(window)
-button2.configure(textvariable=string, command = lambda: change(amount * -1))
+button2.configure(textvariable=string,command = lambda: change(amount * -1))
 button2.pack(ipady=10, ipadx=200, fill = 'x', padx = 10, pady = 10)
-
+button2.bind('<Enter>',enter)
+button2.bind('<Leave>',leave)
 
 
 
